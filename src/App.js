2dba979698a2dda18bd68d1d98026cb3ea84d140
Michel Ferreira <michel.ferreira.melo@gmail.com>
2022-11-19 04:44:59 -0300

    feat: adicionando o componente CampoTexto

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import Banner from './componentes/Banner/Banner';
+import Banner from './componentes/Banner';
+import CampoTexto from './componentes/CampoTexto';
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      
+      <CampoTexto label="Nome" placeholder="Digite seu nome"/>
+      <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
+      <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
     </div>
   );
 }
