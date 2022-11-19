8e641ee95f410c907e88fdf0d8cd8964395d1155
Michel Ferreira <michel.ferreira.melo@gmail.com>
2022-11-19 19:25:46 -0300

    fix: alterando o valor da props

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -1,12 +1,12 @@
 import './ListaSuspensa.css';
 
-const ListaSuspensa = ({label, itens, obrigatorio, value, aoAlterado}) => {
+const ListaSuspensa = ({label, itens, obrigatorio, valor, aoAlterado}) => {
   return (
     <div className='lista-suspensa'>
       <label>{label}</label>
       <select
         required={obrigatorio}
-        value={value}
+        value={valor}
         onChange={(event) => aoAlterado(event.target.value)}
       >
         { itens.map((item, index) => <option key={index}>{item}</option>) }
